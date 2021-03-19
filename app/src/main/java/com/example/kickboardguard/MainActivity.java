4d1943c1f8afeab7e45604ffd92a7ae64a7f8582
okhi <50296735+denemtia@users.noreply.github.com>
2021-03-19 17:08:17 +0900

    - 레이아웃 추가 - keyHash 값 확인 코드 추가

--- app/src/main/java/com/example/kickboardguard/MainActivity.java
+++ app/src/main/java/com/example/kickboardguard/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.kickboardguard;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         final String[] items = {"WHITE", "RED", "GREEN", "BLUE", "BLACK"} ;
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items) ;
-
+        Log.e("getKeyHash", ""+getKeyHash(MainActivity.this));
         listview = (ListView) findViewById(R.id.drawer_menulist) ;
         listview.setAdapter(adapter) ;
 
@@ -65,5 +73,31 @@ public class MainActivity extends AppCompatActivity {
                 drawer.closeDrawer(Gravity.LEFT) ;
             }
         });
+
     }
+
+
+    //해쉬키 값 얻기
+    public static String getKeyHash(final Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            if (packageInfo == null)
+                return null;
+
+            for (Signature signature : packageInfo.signatures) {
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return android.util.Base64.encodeToString(md.digest(), android.util.Base64.NO_WRAP);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
