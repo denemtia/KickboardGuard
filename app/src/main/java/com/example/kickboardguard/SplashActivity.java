d98fba44283ecc341310641dd0e41d7405658c37
82105 <tmdrl417@naver.com>
2021-03-23 02:03:54 +0900

    -4초간 그림보여주며 로딩후 어플 진입 가능 단 manifest에서 login이랑 겹치기 때문에 수정해야함

---
+++ app/src/main/java/com/example/kickboardguard/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.kickboardguard;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(8000); // splash 화면 대기 시간
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, MainActivity.class)); // splash 화면이 끝난 뒤 띄울 Activity
+
+        finish();
+    }
+}
